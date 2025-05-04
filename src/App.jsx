b7590a91e4271a3437d8a3e3d4b5e2b5c43cb8f0
PatrickRuggeri <patrickofca@gmail.com>
2025-05-04 16:13:44 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigation } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return <></>;
+}
+
+export default App;
